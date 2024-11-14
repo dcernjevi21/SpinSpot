0f9ba1a6b1345f7dea9e3960028a231a4f54db34
nhuzjak20 <huzjakn5@gmail.com>
2024-11-14 18:57:08 +0100

    Search Adapter dodan, mock Data za Djeve dodana,

--- Software/app/src/main/java/com/example/bassbytecreators/MainActivity.kt
+++ Software/app/src/main/java/com/example/bassbytecreators/MainActivity.kt
@@ -10,11 +10,18 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bassbytecreators.adapters.SearchAdapter
+import com.example.bassbytecreators.entities.DJperson
 import com.example.bassbytecreators.helpers.AddGigDialogHelper
+import com.example.bassbytecreators.helpers.MockDataLoader
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.Console
 
 class MainActivity : AppCompatActivity() {
+    lateinit var djRecyclerView : RecyclerView
+    var djList : List<DJperson> = MockDataLoader().getDemoDJdata()
+    lateinit var searchAdapter: SearchAdapter
 
     //za dodavanje gaže
     private lateinit var btnAddGig: FloatingActionButton
@@ -28,6 +35,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())

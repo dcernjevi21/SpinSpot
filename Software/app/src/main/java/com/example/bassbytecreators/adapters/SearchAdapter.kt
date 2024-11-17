29a0d43a0e2f153b1bf5814a4494214424a946b4
nhuzjak20 <huzjakn5@gmail.com>
2024-11-17 12:17:04 +0100

    Napokon radi implementacija searcha

--- Software/app/src/main/java/com/example/bassbytecreators/adapters/SearchAdapter.kt
+++ Software/app/src/main/java/com/example/bassbytecreators/adapters/SearchAdapter.kt
@@ -6,32 +6,29 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bassbytecreators.R
-import com.example.bassbytecreators.adapters.SearchAdapter.SearchViewHolder
+import com.example.bassbytecreators.adapters.SearchAdapter.DJViewHolder
 import com.example.bassbytecreators.entities.DJperson
 
-class SearchAdapter(private var DJlist : ArrayList<DJperson> ): RecyclerView.Adapter<SearchViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.dj_search_item, parent, false)
-        return SearchViewHolder(itemView)
+class SearchAdapter(private var djList: List<DJperson>):  RecyclerView.Adapter<SearchAdapter.DJViewHolder>(){
+    inner class DJViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nazivTextView: TextView = view.findViewById(R.id.textView8)
+        val zanrTextView: TextView = view.findViewById(R.id.textView9)
     }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DJViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.dj_search_item, parent, false)
+        return DJViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: DJViewHolder, position: Int) {
+        val dj = djList[position]
+        holder.nazivTextView.text = dj.name
+        holder.zanrTextView.text = dj.genre
 
-    fun filtriraj(filterList: ArrayList<DJperson>) {
-        DJlist = filterList
+    }
+    override fun getItemCount(): Int = djList.size
+
+    fun updateList(newList: List<DJperson>) {
+        djList = newList
         notifyDataSetChanged()
-    }
-
-    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
-        holder.djName.text = DJlist.get(position).name
-        holder.djGenre.text = DJlist.get(position).genre
-
-    }
-
-    override fun getItemCount(): Int {
-        return DJlist.size
-    }
-    inner class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val djName : TextView = itemView.findViewById(R.id.textView8)
-        val djGenre : TextView = itemView.findViewById(R.id.textView9)
     }
 
 }

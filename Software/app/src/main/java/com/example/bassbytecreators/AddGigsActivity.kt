78f937d15daef2c5c675c6032c01d936d40dfe21
Dominik Cernjevic <126580829+dcernjevi21@users.noreply.github.com>
2025-01-20 18:56:08 +0100

    Revert "f07_notifikacije_korisnicima into dev"

--- Software/app/src/main/java/com/example/bassbytecreators/AddGigsActivity.kt
+++ Software/app/src/main/java/com/example/bassbytecreators/AddGigsActivity.kt
@@ -12,6 +12,7 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -44,6 +45,7 @@ class AddGigsActivity : AppCompatActivity() {
     private lateinit var txt5: TextView
     private lateinit var txt6: TextView
     private lateinit var txt7: TextView
+    private lateinit var txt8: TextView
     private lateinit var txt10: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -163,7 +165,6 @@ class AddGigsActivity : AppCompatActivity() {
         menu.findItem(R.id.nav_login)?.isVisible = false
         menu.findItem(R.id.nav_registration)?.isVisible = false
         menu.findItem(R.id.nav_djstatistics)?.isVisible = true
-        menu.findItem(R.id.nav_addgigs)?.isVisible = false
 
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -177,7 +178,16 @@ class AddGigsActivity : AppCompatActivity() {
                     val userId = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
                         .getInt("logged_in_user_id", -1) // Dohvati userId
                     intent.putExtra("user_id", userId) // Proslijedi userId
-                    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                    startActivity(intent)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+
+                R.id.nav_addgigs -> {
+                    val intent = Intent(this, AddGigsActivity::class.java)
+                    val userId = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+                        .getInt("logged_in_user_id", -1) // Dohvati userId
+                    intent.putExtra("user_id", userId) // Proslijedi userId
                     startActivity(intent)
                     drawerLayout.closeDrawers()
                     true
